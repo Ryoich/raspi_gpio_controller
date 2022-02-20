af3f1b559f21eb01eee854fc0b2cbc4aabdb8a00
ryo ohkubo <ryoohkubo@Air.local>
2022-02-20 17:32:36 +0900

    fix Mycontroller Class

--- modular/PS4Controller.py
+++ modular/PS4Controller.py
@@ -1,18 +1,18 @@
 from raspberrypi_gpio import My_Raspi_GPIO_Controller
 from pyPS4Controller.controller import Controller
 
-class MyController(Controller, My_Raspi_GPIO_Controller):
+class MyController(Controller):
     
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
-        My_Raspi_GPIO_Controller.__init__(self)
+        self.raspi_gpio = My_Raspi_GPIO_Controller();
     
     def on_x_press(self):
         print("Now I press the x button")
-        self.go_forward()
+        self.raspi_gpio.go_forward()
 
     def on_x_press(self):
         print("Now I release the x button")
-        self.stop()
+        self.raspi_gpio.stop()
 
 
